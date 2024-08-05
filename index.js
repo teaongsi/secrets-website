7d965813e3be4808e3651fc36dfe0b11a2654e23
teaongsi <154658746+teaongsi@users.noreply.github.com>
2024-08-05 19:10:33 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var pass = "";
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+function passwordCheck(req ,res ,next){
+    console.log(req.body);
+    pass = req.body["password"];
+    next();
+}
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    if(pass === "Spillag3") {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
